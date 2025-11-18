b82c463a750b2262c75236371bc1b15c24b10ec1
Bruno Lallo <brunolallo8@gmail.com>
2025-11-18 16:17:30 -0300

    Feat: Página de administrador feita, necessário corrigir o carregamento assíncrono dos técnicos e o cadastro

--- Presentation/ViewModels/AdminViewModel.cs
+++ Presentation/ViewModels/AdminViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Data;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SisCras.ApplicationLayer.Services;
@@ -14,6 +15,8 @@ public partial class AdminViewModel : ObservableObject
 
     [ObservableProperty] private Tecnico? _tecnicoLogado = new();
     [ObservableProperty] private Tecnico _novoTecnico = new();
+    [ObservableProperty] private string _confirmarSenha = String.Empty;
+    [ObservableProperty] private bool _buscandoTecnicos = false;
     public ObservableCollection<Tecnico> Tecnicos { get; set; } = [];
 
     public AdminViewModel(ILoggedUserService loggedUserService, ICrasService crasService, ITecnicoService tecnicoService)
@@ -27,12 +30,23 @@ public partial class AdminViewModel : ObservableObject
 
     public async Task BuscarTecnicos()
     {
+        BuscandoTecnicos = false;
         Tecnicos = new(await _crasService.GetTecnicosFromCras(TecnicoLogado.CrasAtivo));
+        BuscandoTecnicos = true;
     }
     
     [RelayCommand]
     private Task AdicionarTecnico()
     {
+        NovoTecnico.TecnicoCras.Add(new()
+        {
+            Id = 0,
+            CrasId = TecnicoLogado.CrasAtivo.Id,
+            Cras = TecnicoLogado.CrasAtivo,
+            Tecnico = NovoTecnico,
+            TecnicoId = 0,
+            DataEntrada = DateOnly.FromDateTime(DateTime.Now)
+        });
         return _tecnicoService.AddAsync(NovoTecnico);
     }
     
