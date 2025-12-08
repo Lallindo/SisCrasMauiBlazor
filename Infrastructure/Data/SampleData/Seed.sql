3c38e45a941109f83904b8cc53ca3a57e3ccc9a5
Bruno Lallo <brunolallo8@gmail.com>
2025-12-08 13:29:05 -0300

    Fix: Correção do Seed.sql e do botão de adicionar novo membro

--- Infrastructure/Data/SampleData/Seed.sql
+++ Infrastructure/Data/SampleData/Seed.sql
@@ -17,21 +17,26 @@ Values ('Ana', '12345678910', '04-08-2002', 1, 2,
         'Programador', 1, 1000.00, '12345678912', 1),
        ('João', '12345678913', '02-17-1998', 1, 2,
         1, '12345678914', '', 'Testador', 1,
-        'Testador', 3, 2000.00, '12345678915', 0);
+        'Testador', 3, 2000.00, '12345678915', 0),
+       ('Maria', '12345678916', '12-23-1997', 5, 1,
+        1, '12345678917', '', 'Analista', 3,
+        'Analista', 2, 3000.00, '12345678918', 1);
 
 INSERT INTO Familias(ConfiguracaoFamiliar)
 VALUES (1),
        (0),
-       (3);
+       (3),
+       (2);
 
 INSERT INTO Prontuarios(Codigo, CrasId, DataCriacao, FamiliaId, FormaDeAcesso, TecnicoId)
 VALUES (1, 1, '03-11-2025', 1, 'Testando', 1),
        (2, 2, '04-10-2025', 2, 'Testando2', 2);
 
-INSERT INTO TecnicoCras(CrasId, DataEntrada, TecnicoId)
-VALUES (1, '04-11-2023', 1),
-       (2, '16-10-2022', 2);
+INSERT INTO TecnicoCras(CrasId, DataEntrada, TecnicoId, Admin)
+VALUES (1, '04-11-2023', 1, 1),
+       (2, '16-10-2022', 2, 1);
 
-INSERT INTO FamiliaUsuarios(Familiaid, Parentesco, UsuarioId)
-VALUES ( 1, 'T1', 1),
-       ( 1, 'T2', 2);
+INSERT INTO FamiliaUsuarios(Familiaid, Parentesco, UsuarioId, DataAdicao)
+VALUES ( 1, 1, 1, '12-08-2025'),
+       ( 1, 2, 2, '12-05-2025'),
+       (2, 1, 3, '12-08-2025');
